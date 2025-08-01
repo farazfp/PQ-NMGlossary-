deb42e6a7540eb5c88878f7967a98b299bfc21aa
farazfp <faraz.fp@gmail.com>
2025-08-01 00:44:41 -0700

    Quartz sync: Aug 1, 2025, 12:44 AM

--- quartz/components/pages/graphonly.tsx
+++ quartz/components/pages/graphonly.tsx
@@ -2,14 +2,18 @@ import { QuartzComponentConstructor } from "../types";
 import { Graph } from "./Graph";
 import "../../quartz/components/styles/global.scss";
 import "../../quartz/components/styles/graph.scss";
+
 export const GraphOnly: QuartzComponentConstructor = () => (
-  <main style={{
-    width: "100vw",
-    height: "100vh",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center"
-  }}>
+  <main
+    style={{
+      width: "100vw",
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      background: "#fff" // White background; adjust as needed
+    }}
+  >
     <div style={{ width: "90vw", height: "90vh" }}>
       <Graph />
     </div>
